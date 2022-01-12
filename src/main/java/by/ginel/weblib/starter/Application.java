faced58cf26fa01b4862e9a615fd95f3f6350acc
Moonshine-sh <66140132+Moonshine-sh@users.noreply.github.com>
2022-01-12 14:53:21 +0300

    Dao fix

---
+++ src/main/java/by/ginel/weblib/starter/Application.java
@@ -0,0 +1,9 @@
+package by.ginel.weblib.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+class Application {
+    public static void main(String[] args) { SpringApplication.run(Application.class, args); }
+}
